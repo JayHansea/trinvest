9beb5ac784fa947ec68960733db8dbff518a2589
JayHansea <ezeokafortochukwu@gmail.com>
2024-04-20 23:51:55 +0100

    bottom banner

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Pricing from "@/components/Pricing";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import BottomBanner from "@/components/BottomBanner";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
       <Pricing />
       <FAQs />
       <Contact />
+      <BottomBanner />
       <Footer />
     </main>
   );
